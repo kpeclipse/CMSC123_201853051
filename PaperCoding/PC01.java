7d25555f5a427c15e44769c2a459e710e1ec5fcb
kpeclipse <58826783+kpeclipse@users.noreply.github.com>
2020-02-07 15:26:11 +0800

    PC01_FINAL
    triangle function

---
+++ PaperCoding/PC01.java
@@ -0,0 +1,29 @@
+package PaperCoding;
+import java.util.*;
+
+public class PC01{
+    int input;
+    Scanner scan = new Scanner(System.in);
+
+    public PC01(){
+        System.out.print("Input: ");
+        input = scan.nextInt();
+        triangle(input);
+    }
+
+    public void triangle(int k){
+        for(int i = 0; i <= k; i++){
+            for(int j = k; j > i; j--)
+                System.out.print(" ");
+            for(int z = 1; z <= i; z++)
+                System.out.print("* ");
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new PC01();
+    }
+
+    
+}
