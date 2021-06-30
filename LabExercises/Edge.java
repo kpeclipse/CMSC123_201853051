5e129bcde2c39f40f475d316136722ee99d0903b
kpeclipse <58826783+kpeclipse@users.noreply.github.com>
2021-06-30 19:38:52 +0800

    Lab Exercises moved to new folder
    Moved lab exercises in one folder

--- LabExercises/Edge.java
+++ LabExercises/Edge.java
@@ -1,17 +1,17 @@
-class Edge{
+class Edge {
     Vertex first;
     Vertex second;
     double value;
 
     // With Weight
-    public Edge(Vertex f, Vertex s, double v){
+    public Edge(Vertex f, Vertex s, double v) {
         first = f;
         second = s;
         value = v;
     }
 
     // Without Weight
-    public Edge(Vertex f, Vertex s){
+    public Edge(Vertex f, Vertex s) {
         first = f;
         second = s;
     }
